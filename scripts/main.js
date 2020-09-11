aa1c20cac999abdac13f0579c5c8a797f4f84148
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-09-11 09:52:13 -0500

    forgot to push on day i was working on API

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
-const jokeBtn = document.querySelector(".request-joke-btn");
+import {makeJoke, useJoke} from "./JokeProvider.js"
+import {Joke, PunchLine} from "./Joke.js"
 
+const jokeBtn = document.querySelector(".request-joke-btn");
+const punchlineBtn = document.querySelector(".request-punch-btn");
+const jokeElement = document.querySelector(".joke-setup");
+const punchlineElement = document.querySelector(".joke-punchline");
+const clearingElement =
 jokeBtn.addEventListener("click", (e) => {
-  console.log("I should be grabbing a joke now...");
+  makeJoke().then(jokeData => {
+
+    const theJoke = useJoke()
+      jokeElement.innerHTML = Joke(theJoke)
+      
+    punchlineBtn.addEventListener("click", () => {
+      makeJoke().then(jokeData => {
+        punchlineElement.innerHTML = PunchLine(theJoke)
+      })
+    })
+  })
+  
   // TODO: fetch a joke from the Joke API and render it to the DOM
-});
+})
+
+
